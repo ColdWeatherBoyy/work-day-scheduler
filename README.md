b8f652278e810fc5c97de1a83d41232910b6c681
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-02-15 19:31:01 -0500

    adds final touches, comments, and the beginnings of README which needs a deployment link

---
+++ README.md
@@ -0,0 +1,24 @@
+# Work Scheduler
+
+## Description
+
+This repo demonstrates a single-day work scheduler that shows the current date, the working hours of that date (color coded for past, present, and future), and allows the user to save events for particular hour-long time slots in local storage that will continue to populate the scheduler on each reload. This allowed me to gain greater practice in my use of JavaScript – also in jQuery, though generally vanilla JS seemed to feel more intuitive to me. One jQuery solution for a particular part of the page functionality exists and has been commented out. 
+
+Though there is a heavy debate among developers over whether jQuery should still be in use, it obviously still has a massive user base. However, its use moving forward seems to be on the decline, or at least new tools are available that now can be used in its place (conversations about or evidence of this can be seen [here](https://medium.com/@apoorva72900/why-is-jquery-dead-b41dba56491e#:~:text=JQuery%20is%20useful%2C%20but%20what,So%2C%20it%20started%20becoming%20obsolete.), [here](https://thenewstack.io/why-outdated-jquery-is-still-the-dominant-javascript-library/), and [here](https://youmightnotneedjquery.com/)). Given this, and as I still more naturally think in vanilla JS (and am still trying to build my skills with it), much of this was accomplished using vanilla JS instead, as much of jQueries initial benefits are now part of vanilla JS.
+
+## Installation
+
+No installation necessary.
+
+## Usage
+
+Just visit the page here [here](https://coldweatherboyy.github.io/Code-Quiz/), click the Start Quiz button, and enjoy!
+
+Additional questions can be added in a fairly straightforward fashion as desired. Recommended to update total timer time if so.
+
+You can view the two pages below: 
+![screenshot of scheduler](assets/images/MainPage.png)
+
+## Credits
+
+Second eyes, debugging help, and moral support from [Antal Spector-Zabusky](https://gitlab.com/antalsz).
--- script.js
+++ script.js
@@ -1,59 +1,52 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 
-// set this as window load
-// $(document).ready(function() {
-window.onload = function () {
+// originally used similar vanilla JS  window.onload = function () {, but switched to jQuery method to make sure browswer had finished rendering all elements
+$(document).ready(function() {
   let currentDate = dayjs();
   let currentHour = currentDate.hour();
   var currentDay = document.querySelector("#currentDay");
   var hourBlocks = document.querySelectorAll(".time-block");
 
 
-  // can't get the o to fucking work. Adds format to p tag
+  // Adds text content to title of page with todays date
   currentDay.textContent = "Today is the " + currentDate.format("MMM DD, 'YY")
 
   // checks if there is stored value, and if so, prints it in appropriate slot
   for (let z = 0; z < hourBlocks.length; z++) {
     var savedEventInfo = localStorage.getItem(hourBlocks[z].getAttribute("id"));
     var textAreaArr = document.querySelectorAll("textarea");
-    console.log(savedEventInfo);
     if (savedEventInfo !== null) {
       textAreaArr[z].textContent = savedEventInfo;      
     }
   }
 
   // use currentHour to validate against time listed in calendar and apply classes as necessary
-
+  // cycles through all time blocks
   for (let i = 0; i < hourBlocks.length; i++) {
+
+    // probably doesn't need to be its own function, but separates id attribute at the - to utilize numbers only
+    function getHour(string) {
+      let checkHour = string.split("-")[1];
+      return(Number(checkHour));
+    }
+
+    // utilizes above function to assign number/hour value to each timeslot for validation against current time
     idCheck = hourBlocks[i].getAttribute("id");
     let specificHour = getHour(idCheck);
 
     function addClass(event) {
       hourBlocks[i].classList.add(event);
     }
-
+    // checks current actual hour against hour values of scheduler
     if (specificHour === currentHour) {
       addClass("present");
     } else if (specificHour < currentHour) {
@@ -63,26 +56,33 @@ window.onload = function () {
     }
   }
 
-  // make an event listener for save button click in localStorage.
+  // utilizes forEach function for an array of all buttons to add event listeners to each one
   var btnsArr = document.querySelectorAll(".saveBtn");
   
+
   btnsArr.forEach(function(btn) {
     btn.addEventListener("click", function(event){
+      // defines as variables the sibling and parents needed to identify the proper timeslot and value typed in by user
       let scheduledEv = event.target.previousElementSibling.value;
       let evTime = event.target.parentElement.getAttribute("id");
-      // saves to localStorage the savedEvent with reference to its time and the value of the saved content
+
+      // saves to localStorage the savedEvent with key referring to the time and the value being the content to save
       localStorage.setItem(evTime, scheduledEv);
     });
   });
-}
+    // could be accomplished in jQuery utilizing a similar array and using 'this' to help locate the click and the appropriate surrounding elements rather than event.target. This method can be seen below:
 
-function getHour(string) {
-  let checkHour = string.split("-")[1];
-  return(Number(checkHour));
-}
-
-
-  //eliinate for loop with this map function, requires an array conversion
-    // var allIDs = Array.from(hourBlocks).map(function(elem){
-      //   return elem.getAttribute("id");
-      // })
+      // var $btnsArr = $(".saveBtn");
+      
+      // $btnsArr.each(function() {
+      //   $(this).on("click", function() {
+      //     defines as variables the sibling and parents needed to identify the proper timeslot and value typed in by user
+      //     let scheduledEv = $(this).prev().val();
+      //     let evTime = $(this).parent().attr("id");
+      
+      //     saves to localStorage the savedEvent with key referring to the time and the value being the content to save
+      //     localStorage.setItem(evTime, scheduledEv);
+      //   });
+      // });
+  
+});
